ff397c6bb1a71f78ec02d86da537362555eef31c
Sébastien Barbier <contact@sebastienbarbier.com>
2020-04-07 21:21:28 +0200

    Migrate sebastienbarbier.com to ovh hosting

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 # domains=(sebastienbarbier.com www.sebastienbarbier.com)
-domains=(ovh.sebastienbarbier.com whereis.sebastienbarbier.com)
+domains=(sebastienbarbier.com whereis.sebastienbarbier.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="contact@sebastienbarbier.com" # Adding a valid address is strongly recommended
