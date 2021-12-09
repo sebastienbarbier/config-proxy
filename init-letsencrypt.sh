9d529bd213985b84334a1b8e937b9fd113061a61
Sébastien Barbier <contact@sebastienbarbier.com>
2021-12-09 17:24:42 +0000

    Add status.seven23.io within proxy conf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 # domains=(sebastienbarbier.com www.sebastienbarbier.com)
-domains=(sebastienbarbier.com whereis.sebastienbarbier.com sebastienbarbier.fr app.seven23.io dns.sebastienbarbier.com status.sebastienbarbier.com)
+domains=(sebastienbarbier.com whereis.sebastienbarbier.com sebastienbarbier.fr app.seven23.io dns.sebastienbarbier.com status.sebastienbarbier.com status.seven23.io)
 rsa_key_size=4096
 data_path="./certbot"
 email="contact@sebastienbarbier.com" # Adding a valid address is strongly recommended
